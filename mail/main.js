97572c398cf05ad498e43f089e3ae52cea6401ec
federicoalagna <federicoalagna01@gmail.com>
2024-05-02 14:45:02 +0200

    email autorizzate

--- mail/main.js
+++ mail/main.js
@@ -3,6 +3,13 @@
 
 //chiedo all'utente di inserire la sua email utilizzando la funzione prompt
 const userEmail = prompt("Inserisci la tua email:");
+console.log("Email inserita: " + userEmail);
 
 //--creo una lista di email di chi può accedere al programma (array)
 const listaEmail = ["aldo@gmail.com", "giovanni@gmail.com", "giacomo@gmail.com"];
+
+//creo un ciclo per stabilire tutte le email autorizzate in console
+console.log("Lista email di chi può accedere:");
+for (let i = 0; i < listaEmail.length; i++) {
+    console.log(listaEmail[i]);
+}
