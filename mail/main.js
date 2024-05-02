f6df787cce786a7904961855b35a174b063ceae1
federicoalagna <federicoalagna01@gmail.com>
2024-05-02 14:38:22 +0200

    email prompt

--- mail/main.js
+++ mail/main.js
@@ -1,1 +1,8 @@
 'use strict'
+
+
+//chiedo all'utente di inserire la sua email utilizzando la funzione prompt
+const userEmail = prompt("Inserisci la tua email:");
+
+//--creo una lista di email di chi può accedere al programma (array)
+const listaEmail = ["aldo@gmail.com", "giovanni@gmail.com", "giacomo@gmail.com"];
